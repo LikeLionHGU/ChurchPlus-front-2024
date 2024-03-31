8f1112004579d97d5b06f03f1c8b7df8f6b715ee
dkrehd0519 <dkrehd0519@naver.com>
2024-04-01 01:15:12 +0900

    feat: recoil setting 및 route setting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-// import { RecoilRoot } from "recoil";
-import InputGroupName from "./pages/CreateGroupPage/InputGroupName";
-import InputUserName from "./pages/CreateGroupPage/InputUserName";
-import InputPosition from "./pages/CreateGroupPage/InputPosition";
-import InputGroupCode from "./pages/CreateGroupPage/InputGroupCode";
+import { RecoilRoot } from "recoil";
+import InputGroupName from "./Pages/CreateGroupPage/InputGroupName";
+import InputUserName from "./Pages/CreateGroupPage/InputUserName";
+import InputPosition from "./Pages/CreateGroupPage/InputPosition";
+import InputGroupCode from "./Pages/CreateGroupPage/InputGroupCode";
+import Homepage from "./Pages/HomePage/HomePage";
+import LoginPage from "./Pages/HomePage/LoginPage/LoginPage";
+// import LoginPage from "./Pages/HomePage/LoginPage/LoginPage";
+import Loading from "./Pages/HomePage/LoginPage/Loading";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SelectTeamPage from "./Pages/HomePage/SelectTeamPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/InputGroupCode" element={<InputGroupCode />}></Route>
-        <Route path="/InputGroupName" element={<InputGroupName />}></Route>
-        <Route path="/InputUserName" element={<InputUserName />}></Route>
-        <Route path="/InputPosition" element={<InputPosition />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/InputGroupCode" element={<InputGroupCode />}></Route>
+          <Route path="/InputGroupName" element={<InputGroupName />}></Route>
+          <Route path="/InputUserName" element={<InputUserName />}></Route>
+          <Route path="/InputPosition" element={<InputPosition />}></Route>
+          <Route path="/LoginPage" element={<LoginPage />}></Route>
+          {/* <Route path="/LoginPage" element={<LoginPage />}></Route> */}
+          <Route path="/loading" element={<Loading />}></Route>
+          <Route path="/SelectTeamPage" element={<SelectTeamPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
