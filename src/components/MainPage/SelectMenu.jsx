e2997518797e7e79362662f669e56d06b2df4251
JongHyun <jonghyun@JongHyuns-MacBook-Pro.local>
2024-05-08 11:44:27 +0900

    feat: main page 살짝 수정

--- src/components/MainPage/SelectMenu.jsx
+++ src/components/MainPage/SelectMenu.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import MainPageIcon from "../../asset/Images/Icons/MainPageIcon.svg"
+import ContiCreateIcon from "../../asset/Images/Icons/ContiCreate.svg"
+import ContiStorageIcon from "../../asset/Images/Icons/ContiStorage.svg"
+import ManageGroup from "../../asset/Images/Icons/ManageGroup.svg"
+import TeamDropdownIcon from "../../asset/Images/Icons/TeamDropdownIcon.svg"
 
 const Wrapper = styled.div`
   display: flex;
@@ -14,60 +19,110 @@ const Wrapper = styled.div`
   /* border: 2px solid green; */
 `;
 
-const WeekSheet = styled.button`
+const TeamDropdown = styled.div`
   font-family: "GmarketSansMedium";
   cursor: pointer;
   width: 182px;
   height: 43px;
-  background-color: rgba(204, 223, 255, 0.5);
-  border: none;
-  border-radius: 10px;
-  font-size: 20px;
+  /* background-color: rgba(204, 223, 255, 0.5); */
+  /* border: none; */
+  /* border-radius: 10px; */
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 18px;
+  margin-bottom: 11px;
+  margin-right: 10px;
 `;
 
 const Dropdown = styled.div`
   width: 182px;
 `;
 
-const DropdownItem = styled.div`
-  margin: 28px 0px 28px 0px;
-
-  div {
-    cursor: pointer;
-    padding-left: 11px;
-    padding-top: 7px;
-    padding-bottom: 7px;
-    font-size: 20px;
-    margin-bottom: 14px;
-  }
-
-  div:hover {
+const Items = styled.div`
+  font-family: "GmarketSansMidium";
+  /* margin-top: 5px; */
+  cursor: pointer;
+  padding-left: 9px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  font-size: 14px;
+  margin-left: 5px;
+  color: #3e5692;
+  display: flex;
+  align-items: center;
+  &:hover {
     background-color: #eeeeee;
     border-radius: 10px;
+    font-weight: 700;
   }
-`;
+  img{
+    padding-right: 10px;
+  }
+  span{
+    margin-top: 3px;
+  }
+`
+
+// const DropdownItem = styled.div`
+//   margin: 28px 0px 28px 0px;
+
+//   div {
+//     cursor: pointer;
+//     padding-left: 11px;
+//     padding-top: 7px;
+//     padding-bottom: 7px;
+//     font-size: 20px;
+//     margin-bottom: 14px;
+//   }
+
+//   div:hover {
+//     background-color: #eeeeee;
+//     border-radius: 10px;
+//   }
+// `;
 
 function SelectMenu() {
-  const options = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-  ];
+//   const options = [
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "11",
+//     "12",
+//   ];
 
   return (
     <Wrapper>
-      <WeekSheet>이번주 콘티</WeekSheet>
+      <TeamDropdown>
+        글로리아 팀
+        <img src={TeamDropdownIcon} alt=""/>  
+      </TeamDropdown>
       <Dropdown>
-        <DropdownItem>
+        <Items>
+          <img src={MainPageIcon} alt=""/>
+          <span>메인 페이지</span>
+        </Items>
+        <Items>
+          <img src={ContiCreateIcon} alt=""/>
+          <span>콘티 생성하기</span>
+        </Items>
+        <Items>
+          <img src={ContiStorageIcon} alt=""/>
+          <span>콘티 보관하기</span>
+        </Items>
+        <Items>
+          <img src={ManageGroup} alt=""/>
+          <span>팀 관리</span>
+        </Items>
+        {/* <DropdownItem>
           {options.map((option) => (
             <Link
               key={option}
@@ -77,7 +132,7 @@ function SelectMenu() {
               <div key={option}>{option + "월"}</div>
             </Link>
           ))}
-        </DropdownItem>
+        </DropdownItem> */}
       </Dropdown>
     </Wrapper>
   );
--- src/components/MainPage/SideBar.jsx
+++ src/components/MainPage/SideBar.jsx
@@ -2,15 +2,28 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import styled from "styled-components";
 import SelectMenu from "./SelectMenu";
+import Logo from "../../assets/logo/GroomLogo.svg";
 
 const Wrapper = styled.div`
   width: 203px;
+`;
+const GroomLogo = styled.div`
+  padding-top: 18px;
+  padding-left: 33px;
+  cursor: pointer;
+  img {
+    height: 45px;
+    width: 133px;
+  }
 `;
 
 function SideBar() {
   return (
     <Wrapper>
-      <UserInfo />
+      {/* <UserInfo /> */}
+      <GroomLogo>
+        <img src={Logo} alt="groom 로고" />
+      </GroomLogo>
       <SelectMenu />
     </Wrapper>
   );
