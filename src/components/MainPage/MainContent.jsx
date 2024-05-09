c91e0a22031b2630ae8edb5fe042a77eb4c085fd
YearaChoi <22200750@handong.ac.kr>
2024-05-10 02:46:54 +0900

    feat: 그리드뷰, 리스트뷰 전환기능 구현

--- src/components/MainPage/MainContent.jsx
+++ src/components/MainPage/MainContent.jsx
@@ -34,13 +34,52 @@ const Icons = styled.div`
 `;
 
 const Contents = styled.div`
-  height: 75%;
-  width: calc(100% - 64px);
+  height: 100vh;
+  width: calc(100% - 20px);
   border: 1px solid #3e5692;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const SheetMusicItem = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const SheetMusicImage = styled.img`
+  width: 270px;
+  height: 184px;
+  margin-right: 20px;
+  margin-bottom: 29px;
+  border-radius: 16px;
+  cursor: pointer;
+`;
+
+const SheetMusicDetails = styled.div`
+  margin-bottom: 10px;
 `;
 
 function MainContent() {
+  const [isListView, setIsListView] = useState(false);
   const [, setSearch] = useState("");
+
+  // 악보이미지 더미
+  const sheetMusicData = [
+    {
+      imageUrl:
+        "https://mblogthumb-phinf.pstatic.net/MjAxODA2MDZfMTc4/MDAxNTI4MjY3MzQ0NDM0.y2pvrCJr79epCxzn2zResj-6HBlmC5FbzH233jBUXZIg.jiikNEE7d1Jg6xQv4eWRyI5C7Zfg1t3ohmQuge-OZMsg.JPEG.lhy21ch/6773cda6c41114d62ed77d8bb8301588.jpg?type=w800",
+      key: "C",
+      title: "주님 말씀하시면",
+      version: "마커스",
+    },
+    {
+      imageUrl:
+        "https://mblogthumb-phinf.pstatic.net/MjAxODA2MDZfMTc4/MDAxNTI4MjY3MzQ0NDM0.y2pvrCJr79epCxzn2zResj-6HBlmC5FbzH233jBUXZIg.jiikNEE7d1Jg6xQv4eWRyI5C7Zfg1t3ohmQuge-OZMsg.JPEG.lhy21ch/6773cda6c41114d62ed77d8bb8301588.jpg?type=w800",
+      key: "C",
+      title: "기쁨의 옷을 입은",
+      version: "어노인팅",
+    },
+  ];
 
   return (
     <Wrapper>
@@ -51,11 +90,37 @@ function MainContent() {
         <UploadMusicModal />
 
         <Icons>
-          <img src={viewListIcon} alt="리스트뷰 아이콘"></img>
-          <img src={viewGridIcon} alt="그리드뷰 아이콘"></img>
+          <img
+            src={viewListIcon}
+            alt="리스트뷰 아이콘"
+            onClick={() => setIsListView(true)}
+          />
+          <img
+            src={viewGridIcon}
+            alt="그리드뷰 아이콘"
+            onClick={() => setIsListView(false)}
+          />
         </Icons>
       </FunctionWrapper>
-      <Contents>폴더, 악보</Contents>
+      <Contents>
+        {isListView
+          ? sheetMusicData.map((sheetMusic, index) => (
+              <SheetMusicItem key={index}>
+                <SheetMusicDetails>
+                  <p>이름: {sheetMusic.title}</p>
+                  <p>곡 버전: {sheetMusic.version}</p>
+                  <p>Key: {sheetMusic.key}</p>
+                </SheetMusicDetails>
+              </SheetMusicItem>
+            ))
+          : sheetMusicData.map((sheetMusic, index) => (
+              <SheetMusicImage
+                key={index}
+                src={sheetMusic.imageUrl}
+                alt={`악보 이미지 ${index}`}
+              />
+            ))}
+      </Contents>
     </Wrapper>
   );
 }
