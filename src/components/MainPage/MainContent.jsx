b0bc6f15b87b7329c30748f2c7c6c6bf6475fd26
YearaChoi <22200750@handong.ac.kr>
2024-05-10 02:07:10 +0900

    add: 악보 보관함 페이지 리스트, 그리드 아이콘 추가

--- src/components/MainPage/MainContent.jsx
+++ src/components/MainPage/MainContent.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import SearchBar from "./SearchBar";
 import KeySelectDropdown from "./KeySelectDropdown";
 import UploadMusicModal from "../../pages/MainPage/UploadMusicModal";
+import viewListIcon from "../../assets/Icons/viewList.svg";
+import viewGridIcon from "../../assets/Icons/viewGrid.svg";
 
 const Wrapper = styled.div`
   /* border: 2px solid pink; */
@@ -15,8 +17,20 @@ const FunctionWrapper = styled.div`
   display: flex;
   height: 42px;
   margin-bottom: 34px;
-  width: calc(100% - 64px);
+  width: calc(100% - 20px);
   /* border: 1px solid #3e5692; */
+`;
+
+const Icons = styled.div`
+  padding-left: 233px;
+  padding-top: 9px;
+  /* border: 1px solid red; */
+  height: 24px;
+
+  img {
+    margin-right: 11px;
+    cursor: pointer;
+  }
 `;
 
 const Contents = styled.div`
@@ -35,6 +49,11 @@ function MainContent() {
         <SearchBar setSearch={setSearch} placeholder="곡 버전 검색" />
         <KeySelectDropdown />
         <UploadMusicModal />
+
+        <Icons>
+          <img src={viewListIcon} alt="리스트뷰 아이콘"></img>
+          <img src={viewGridIcon} alt="그리드뷰 아이콘"></img>
+        </Icons>
       </FunctionWrapper>
       <Contents>폴더, 악보</Contents>
     </Wrapper>
