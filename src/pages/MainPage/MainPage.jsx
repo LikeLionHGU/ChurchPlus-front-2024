238ebcf31d9cbfe33a6144fd3c694b525b04adc2
YearaChoi <22200750@handong.ac.kr>
2024-05-16 03:23:14 +0900

    refactor: Header 컴포넌트, 콘티 보관함, 팀관리, 메인페이지 코드 리팩토링

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import MainContent from "../../components/MainPage/MainContent";
 import SideBar from "../../components/MainPage/SideBar";
 import styled from "styled-components";
-import bellIcon from "../../assets/Icons/bell.svg";
-import settingIcon from "../../assets/Icons/setting.svg";
-import userImgIcon from "../../assets/Icons/userImg.svg";
-import { HeaderWrapper, Menu, PersonInfo, Icons  } from "../../components/CommonUI/Header";
-import { BlueText } from "../../components/CreateGroupPage/Text";
+import Header from "../../components/MainPage/Header";
 
 const Wrapper = styled.div`
   display: flex;
@@ -22,17 +18,7 @@ function MainPage() {
     <Wrapper>
       <SideBar />
       <ContentWrapper>
-      <HeaderWrapper>
-          <Menu>악보 보관함</Menu>
-          <Icons>
-            <img src={bellIcon} alt="벨아이콘"></img>
-            <img src={settingIcon} alt="환경설정 아이콘"></img>
-            <img src={userImgIcon} alt="유저 이미지 아이콘"></img>
-          </Icons>
-          <PersonInfo>
-            김교회 | <BlueText>인도자</BlueText>
-          </PersonInfo>
-        </HeaderWrapper>
+        <Header menu={"악보 보관함"} />
         <MainContent />
       </ContentWrapper>
     </Wrapper>
--- src/pages/MainPage/ManageTeamPage.jsx
+++ src/pages/MainPage/ManageTeamPage.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import ManageTeam from "../../components/MainPage/ManageTeam";
 import SideBar from "../../components/MainPage/SideBar";
 import styled from "styled-components";
-import bellIcon from "../../assets/Icons/bell.svg";
-import settingIcon from "../../assets/Icons/setting.svg";
-import userImgIcon from "../../assets/Icons/userImg.svg";
-import { HeaderWrapper, Menu, PersonInfo, Icons  } from "../../components/CommonUI/Header";
-import { BlueText } from "../../components/CreateGroupPage/Text";
+import Header from "../../components/MainPage/Header";
 
 const Wrapper = styled.div`
   display: flex;
@@ -22,17 +18,7 @@ function ManageTeamPage() {
     <Wrapper>
       <SideBar />
       <ContentWrapper>
-        <HeaderWrapper>
-          <Menu>팀 관리</Menu>
-          <Icons>
-            <img src={bellIcon} alt="벨아이콘"></img>
-            <img src={settingIcon} alt="환경설정 아이콘"></img>
-            <img src={userImgIcon} alt="유저 이미지 아이콘"></img>
-          </Icons>
-          <PersonInfo>
-            김교회 | <BlueText>인도자</BlueText>
-          </PersonInfo>
-        </HeaderWrapper>
+        <Header menu={"팀 관리"} />
         <ManageTeam />
       </ContentWrapper>
     </Wrapper>
