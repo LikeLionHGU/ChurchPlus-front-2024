e0cefff81d39f4d703a08973d900c2a41c6c1201
YearaChoi <22200750@handong.ac.kr>
2024-05-27 01:38:24 +0900

    feat: ContiStepModal 페이지네이션 구현

--- src/components/Modal/ContiStepModal.jsx
+++ src/components/Modal/ContiStepModal.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import saveBtn from "../../assets/Icons/SaveBtn.svg";
+import leftArrow from "../../assets/Icons/leftArrow.svg"; // 왼쪽 화살표 아이콘
+import rightArrow from "../../assets/Icons/rightArrow.svg"; // 오른쪽 화살표 아이콘
 import { BlueText } from "../CreateGroupPage/Text";
 
 const modalStyles = `
@@ -35,16 +37,20 @@ const ContiModal = styled.div`
   height: 643px;
 `;
 
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const ModalTop = styled.div`
   display: flex;
-  /* border: 1px solid green; */
   margin-top: 35px;
   margin-bottom: 35px;
   height: 40px;
 `;
 
 const ContiTitle = styled.div`
-  /* border: 1px solid red; */
   margin-left: 480px;
   font-size: 24px;
   font-family: "GmarketSansLight";
@@ -58,15 +64,14 @@ const ContiNumInfo = styled.div`
 
 const ModalContent = styled.div`
   display: flex;
+  position: relative;
 `;
 
 const ContiImage = styled.div`
-  margin-left: 150px;
+  margin-left: 120px;
   margin-right: 90px;
   width: 350px;
   height: 470px;
-
-  /* border: 1px solid #5c39a2; */
 
   img {
     width: 100%;
@@ -78,13 +83,12 @@ const ContiImage = styled.div`
 
 const ContiInfo = styled.div`
   padding-top: 10px;
-  /* border: 1px solid red; */
   width: 419px;
   height: 500px;
 
   img {
     cursor: pointer;
-    margin-left: 355px;
+    margin-left: 340px;
     width: 79px;
     height: 51px;
   }
@@ -92,8 +96,8 @@ const ContiInfo = styled.div`
   textarea {
     font-family: "GmarketSansLight";
     font-size: 16px;
-    height: 80px;
-    width: 300px;
+    height: 90px;
+    width: 280px;
     border: 0.9px solid gray;
     border-radius: 10px;
     resize: none;
@@ -109,6 +113,7 @@ const BoldText = styled.div`
   font-size: 18px;
   padding-bottom: 10px;
 `;
+
 const LightText = styled.div`
   font-family: "GmarketSansLight";
   font-size: 16px;
@@ -120,11 +125,63 @@ const Link = styled.a`
   color: black;
 `;
 
+const Arrow = styled.img`
+  /* border: 1px solid green; */
+  padding: 50px;
+  cursor: pointer;
+  width: 30px;
+  height: 25px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  ${(props) => (props.left ? "left: 20px;" : "right: 20px;")}
+`;
+
 export default function ContiStepModal() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps = [
+    {
+      title: "예수로 살리",
+      code: "G Key",
+      version: "마커스",
+      link: "https://github.com/LikeLionHGU/ChurchPlus-front-2024",
+      imageUrl:
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDEyMTNfNjgg/MDAxNjA3ODA5NjA0NTM2.YM6pUqS1a5iOpS6E6qgmXv-OY2NpxuLqzKKw7zDv8-Yg.YdBycMqSyFsaoOD1SSXqqfYiRucys1Ysb7gN3f7_Eqsg.JPEG.zzseulzz/%EC%98%88%EC%88%98%EB%A1%9C_%EC%82%B4%EB%A6%AC.jpg?type=w800",
+    },
+    {
+      title: "주를 위한 이곳에",
+      code: "C Key",
+      version: "어노인팅",
+      link: "링크 2",
+      imageUrl:
+        "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F2133A53A53E821BC03",
+    },
+    {
+      title: "빛나는 왕의 왕",
+      code: "A Key",
+      version: "위러브",
+      link: "링크 3",
+      imageUrl:
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTEwMDZfMTAg/MDAxNTcwMzM5ODI4MjMx.ZAX16rp89kxvAB6ihoXtCd1ilsstNlPcSONW7TPWj1Qg.N_DHEWNVAPdgz3Oeyr9sXXX5gUFZ_YWPM_AolsYKhuYg.PNG.joseph1040/%EB%B9%9B%EB%82%98%EB%8A%94%EC%99%95%EC%9D%98%EC%99%95.png?type=w800",
+    },
+  ];
 
   const toggleContiStepModal = () => {
     setIsModalOpen((prevState) => !prevState);
+  };
+
+  const handlePrevStep = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const handleNextStep = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep(currentStep + 1);
+    }
   };
 
   useEffect(() => {
@@ -145,39 +202,37 @@ export default function ContiStepModal() {
           <Overlay onClick={toggleContiStepModal} />
           <ContiModal>
             <ModalTop>
-              <ContiTitle>예수로 살리</ContiTitle>
+              <ContiTitle>{steps[currentStep].title}</ContiTitle>
               <ContiNumInfo>
-                현재 <BlueText>2</BlueText>곡이 담겨있어요
+                현재 <BlueText>{steps.length}</BlueText>곡이 담겨있어요
               </ContiNumInfo>
             </ModalTop>
-            <ModalContent>
-              <ContiImage>
-                <img
-                  src="https://mblogthumb-phinf.pstatic.net/MjAyMDEyMTNfNjgg/MDAxNjA3ODA5NjA0NTM2.YM6pUqS1a5iOpS6E6qgmXv-OY2NpxuLqzKKw7zDv8-Yg.YdBycMqSyFsaoOD1SSXqqfYiRucys1Ysb7gN3f7_Eqsg.JPEG.zzseulzz/%EC%98%88%EC%88%98%EB%A1%9C_%EC%82%B4%EB%A6%AC.jpg?type=w800"
-                  alt="Conti Image"
-                />
-              </ContiImage>
-              <ContiInfo>
-                <BoldText>곡 제목</BoldText>
-                <LightText>예수로 살리</LightText>
-                <BoldText>곡 코드</BoldText>
-                <LightText>G Key</LightText>
-                <BoldText>곡 버전</BoldText>
-                <LightText>마커스</LightText>
-                <BoldText>영상 링크</BoldText>
-                <LightText>
-                  <Link
-                    href="https://github.com/LikeLionHGU/ChurchPlus-front-2024"
-                    target="_blank"
-                  >
-                    https://github.com/LikeLionHGU/ChurchPlus-front-2024
-                  </Link>
-                </LightText>
-                <BoldText>메모</BoldText>
-                <textarea placeholder="메모를 자유롭게 입력하세요"></textarea>
-                <img src={saveBtn} alt="저장버튼" />
-              </ContiInfo>
-            </ModalContent>
+            <Flex>
+              <Arrow src={leftArrow} left onClick={handlePrevStep} />
+              <ModalContent>
+                <ContiImage>
+                  <img src={steps[currentStep].imageUrl} alt="Conti Image" />
+                </ContiImage>
+                <ContiInfo>
+                  <BoldText>곡 제목</BoldText>
+                  <LightText>{steps[currentStep].title}</LightText>
+                  <BoldText>곡 코드</BoldText>
+                  <LightText>{steps[currentStep].code}</LightText>
+                  <BoldText>곡 버전</BoldText>
+                  <LightText>{steps[currentStep].version}</LightText>
+                  <BoldText>영상 링크</BoldText>
+                  <LightText>
+                    <Link href={steps[currentStep].link} target="_blank">
+                      {steps[currentStep].link}
+                    </Link>
+                  </LightText>
+                  <BoldText>메모</BoldText>
+                  <textarea placeholder="메모를 자유롭게 입력하세요"></textarea>
+                  <img src={saveBtn} alt="저장버튼" />
+                </ContiInfo>
+              </ModalContent>
+              <Arrow src={rightArrow} onClick={handleNextStep} />
+            </Flex>
           </ContiModal>
         </Modal>
       )}
