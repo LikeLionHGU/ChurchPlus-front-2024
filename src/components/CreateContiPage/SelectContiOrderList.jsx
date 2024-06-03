c3e10d390c15509619ee9e187dea6f99585eba26
YearaChoi <22200750@handong.ac.kr>
2024-06-04 08:40:26 +0900

    style:  체크박스 체크 전, 후 스타일링

--- src/components/CreateContiPage/SelectContiOrderList.jsx
+++ src/components/CreateContiPage/SelectContiOrderList.jsx
@@ -4,6 +4,24 @@ import TableColumn from "../Table/CommonTableColumn";
 import TableRow from "../Table/CommonTableRow";
 import { useRecoilState } from "recoil";
 import { selectedRowsAtom } from "../../recoil/atoms/selectRowsAtom";
+import styled from "styled-components";
+
+const StyleInput = styled.input`
+  margin-right: 20px;
+  appearance: none;
+  cursor: pointer;
+  width: 22px;
+  height: 22px;
+  border: 1.5px solid #f4f4f4;
+  border-radius: 50%;
+  vertical-align: middle;
+  box-shadow: 0 3px 4px rgba(0, 0, 0, 0.05), 0 3px 3px rgba(0, 0, 0, 0.1);
+
+  &:checked {
+    background-color: #aec3de;
+    border: 1.5 solid white;
+  }
+`;
 
 const SelectContiOrderList = ({ sheetMusicData }) => {
   const [selectedRows, setSelectedRows] = useRecoilState(selectedRowsAtom);
@@ -28,7 +46,7 @@ const SelectContiOrderList = ({ sheetMusicData }) => {
             selected={selectedRows.includes(index)}
           >
             <TableColumn>
-              <input
+              <StyleInput
                 type="checkbox"
                 checked={selectedRows.includes(index)}
                 onChange={() => handleCheckboxChange(index)}
